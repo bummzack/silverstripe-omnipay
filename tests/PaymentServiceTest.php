c254751c3928abf6bc70e2aeb611672a08169fc6
Roman Schmid <bummzack@gmail.com>
2016-04-01 16:09:07 +0200

    Added PaymentService Tests

--- tests/PaymentServiceTest.php
+++ tests/PaymentServiceTest.php
@@ -5,13 +5,54 @@ use SilverStripe\Omnipay\Service\ServiceFactory;
 class PaymentServiceTest extends PaymentTest
 {
 
-	public function testRedirectUrl()
+    /** @var \SilverStripe\Omnipay\Service\PurchaseService */
+    protected $service;
+
+    public function setUp()
     {
-		$service = $this->factory->getService(new Payment(), ServiceFactory::INTENT_PURCHASE)
-					->setReturnUrl("abc/123")
-					->setCancelUrl("xyz/blah/2345235?andstuff=124124#hash");
-		$this->assertEquals("abc/123",$service->getReturnUrl());
-		$this->assertEquals("xyz/blah/2345235?andstuff=124124#hash",$service->getCancelUrl());
+        parent::setUp();
+
+        $this->service = $this->factory->getService($this->payment, ServiceFactory::INTENT_PURCHASE);
+    }
+
+    public function testRedirectUrl()
+    {
+		$this->service
+            ->setReturnUrl("abc/123")
+            ->setCancelUrl("xyz/blah/2345235?andstuff=124124#hash");
+
+		$this->assertEquals("abc/123", $this->service->getReturnUrl());
+		$this->assertEquals("xyz/blah/2345235?andstuff=124124#hash", $this->service->getCancelUrl());
 	}
 
+    public function testCancel()
+    {
+        $response = $this->service->cancel();
+
+        $this->assertEquals('Void', $this->payment->Status);
+        $this->assertTrue($response->isCancelled());
+    }
+
+    public function testGateway()
+    {
+        Config::inst()->update('GatewayInfo', 'PaymentExpress_PxPay', array(
+            // set some invalid params
+            'parameters' => array(
+                'DummyParameter' => 'DummyValue'
+            )
+        ));
+
+        $gateway = $this->service->oGateway();
+        $this->assertEquals($gateway->getShortName(), 'Dummy');
+
+        // change the payment gateway
+        $this->payment->Gateway = 'PaymentExpress_PxPay';
+
+        $gateway = $this->service->oGateway();
+        $this->assertEquals($gateway->getShortName(), 'PaymentExpress_PxPay');
+        $this->assertEquals($gateway->getParameters(), array(
+            'username' => 'EXAMPLEUSER',
+            'password' => '235llgwxle4tol23l'
+        ));
+    }
 }
